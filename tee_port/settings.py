0b9568a785627ef56ca92a2854a1b8207ca9ad4b
Queency Tech <ifedolaposamuel2@gmail.com>
2023-08-12 05:25:19 +0100

    added configuration for vercel

--- tee_port/settings.py
+++ tee_port/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -82,12 +82,6 @@ WSGI_APPLICATION = 'tee_port.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
 
